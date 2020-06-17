ea4135ce98467c0b32162152afe0588094d53481
EsraaMamoun <esraamamoun5@gmail.com>
2020-06-18 00:57:13 +0300

    ..

---
+++ challenges/oddNumBst/odd-num-bst.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// const BST = require('../tree/tree.js');
+
+// const newBST = BST.BST();
+// const newBT = BST.BT();
+// const newNode = BST.Node();
+
+const oddNumBst = () => {
+    
+};
+
+// let root = new BST.Node(5);
+// root.left = new BST.Node(2);
+// root.right = new BST.Node(6);
+// root.left.left = new BST.Node(1);
+// root.left.right = new BST.Node(4);
+// root.left.right.left = new BST.Node(3);
+// root.right.right = new BST.Node(8);
+// root.right.right.right = new BST.Node(9);
+// root.right.right.left = new BST.Node(7);
+
+// console.log({root});
+
+module.exports = oddNumBst;
