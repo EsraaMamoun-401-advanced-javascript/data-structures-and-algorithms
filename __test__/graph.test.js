3f6ed29dd56155495311ed86ebd2f4696322fd9a
EsraaMamoun <esraamamoun5@gmail.com>
2020-07-14 13:04:30 +0300

    CC-37

--- __test__/graph.test.js
+++ __test__/graph.test.js
@@ -10,25 +10,32 @@ describe('Graph Class', () => {
   const seven = new Graphs.Vertex(7);
   const eight = new Graphs.Vertex(8);
   const ten = new Graphs.Vertex(10);
+  
   graph.addNode(two);
   graph.addNode(three);
   graph.addNode(six);
   graph.addNode(seven);
   graph.addNode(eight);
   graph.addNode(ten);
-  graph.addEdge(two, seven);
-  graph.addEdge(three, eight);
-  graph.addEdge(six, seven);
-  graph.addEdge(six, eight);
-  graph.addEdge(ten, two);
-  graph.addEdge(ten, three);
-  graph.addEdge(ten, six);
-  graph.addEdge(eight, seven);
-    
+  graph.addEdge(two, seven, 17);
+  graph.addEdge(three, eight, 88);
+  graph.addEdge(six, seven, 6);
+  graph.addEdge(six, eight, 9);
+  graph.addEdge(ten, two, 6);
+  graph.addEdge(ten, three, 33);
+  graph.addEdge(ten, six, 95);
+  graph.addEdge(eight, seven, 79);
+  
   it('Node can be successfully added to the graph', () => {
     expect(graph.adjancyList.has(six)).toBeTruthy();
   });
   it('An edge can be successfully added to the graph', () => {
-    expect(graph.adjancyList.get(two)).toEqual([{'vertice': {'value': 7}, 'weight': 0}]);
+    expect(graph.adjancyList.get(two)).toEqual([{'vertice': {'value': 7}, 'weight': 17}]);
+  });
+  it('Get edge from the graph true', () => {
+    expect(graph.getEdge([eight, seven])).toEqual('true, 79$');
+  });
+  it('Get edge from the graph false', () => {
+    expect(graph.getEdge([eight, two])).toEqual('false, 0$');
   });
 });
